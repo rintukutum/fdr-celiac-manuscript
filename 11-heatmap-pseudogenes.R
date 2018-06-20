e203ce5aa6e957e5742ff286eddfe8743566c3d0
rintukutum <rintukutum@gmail.com>
2018-06-20 22:20:14 +0530

    cluster information added

--- 11-heatmap-pseudogenes.R
+++ 11-heatmap-pseudogenes.R
@@ -36,3 +36,51 @@ generateHeatMap(
   dendo = 'both')
 dev.off()
 
+############################## cluster members manual extraction
+annot.df <- de_probes.pseudo
+gene.symbol <- de_probes.pseudo$name
+idx.gene <- which(annot.df$name %in% gene.symbol)
+probes <- rownames(annot.df)[idx.gene]
+genes <- annot.df[probes,'name']
+mat <- normal_probe_data$df[probes,]
+rownames(mat) <- paste(probes,'.',genes,sep='')
+dat.zScore <- t(apply(mat,1,scale))
+ht2 <- heatmap(dat.zScore)
+genes_order <- sapply(rev(rownames(mat)[ht2$rowInd]),
+	function(x){strsplit(x,split='\\.')[[1]][2]}
+)
+probes_order <- sapply(rev(rownames(mat)[ht2$rowInd]),
+	function(x){strsplit(x,split='\\.')[[1]][1]}
+)
+
+end_cluster_1 <- grep('SRRM1P1',genes_order)
+end_cluster_2 <- grep('RPL13AP5',genes_order)[2]
+end_cluster_3 <- grep('RPS15P4',genes_order)
+end_cluster_4 <- length(genes_order)
+
+cluster_order <- rep(NA,length(genes_order))
+cluster_order[1:end_cluster_1] <- 'A'
+cluster_order[(end_cluster_1+1):end_cluster_2] <- 'B'
+cluster_order[(end_cluster_2+1):end_cluster_3] <- 'C'
+cluster_order[(end_cluster_3+1):end_cluster_4] <- 'D'
+
+cluster_info <- data.frame(
+	probeID = probes_order,
+	geneID = genes_order,
+	cluster = cluster_order,
+	stringsAsFactors = FALSE
+)
+pseudo_gene_anno <- read.csv(
+	'./data/de_probes_pseudo_genes_description.csv',
+	stringsAsFactors=FALSE)
+rownames(pseudo_gene_anno) <- pseudo_gene_anno$probeID
+
+cluster_info_n <- data.frame(
+	cluster_info,
+	description = pseudo_gene_anno[cluster_info$probeID,'description'],
+	stringsAsFactors=FALSE
+)
+write.csv(cluster_info_n,
+	'./data/de_probes_pseudo_genes_cluster_ID_description.csv',
+	row.names=FALSE
+)
